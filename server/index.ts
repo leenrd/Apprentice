786bacfc886fbb642152e36e3c2c5e52ca6a89bc
Leenard <lolleenard0@gmail.com>
2024-02-28 10:50:47 +0800

    chore<server>: src setup

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,4 @@
-import express, { Request, Response } from "express";
-import morgan from "morgan";
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("combined"));
-
-app.get("/", (req: Request, res: Response) => {
-  res.status(200);
-  return res.json({ message: "niggas in paris" });
-});
+import app from "./src/server";
 
 app.listen(5000, () => {
   console.log("listen on port http://localhost:5000");

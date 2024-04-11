886e2b8bca97aa302b8793d539e948cbc296b67b
Leenard <lolleenard0@gmail.com>
2024-04-11 19:23:55 +0800

    fix: imports, validation schemas

--- api/src/auth/userRoute.ts
+++ api/src/auth/userRoute.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-import { check, validationResult } from "express-validator";
 import User from "../models/userModel";
 import {
   bakeCookies,
@@ -7,39 +6,35 @@ import {
   errorValidator,
   hashPassword,
 } from "./userUtils";
-
+import { SIGNUP_VALIDATORS } from "./validationSchema";
 const router = express.Router();
 
-const VALIDATORS = [
-  check("username", "Username is required").isString(),
-  check("password", "Password should be at least 8 characters").isLength({
-    min: 8,
-    max: 20,
-  }),
-];
+router.post(
+  "/signup",
+  SIGNUP_VALIDATORS,
+  async (req: Request, res: Response) => {
+    errorValidator(req, res);
 
-router.post("/signup", VALIDATORS, async (req: Request, res: Response) => {
-  errorValidator(req, res);
+    try {
+      let user = await User.Admin.findOne({
+        username: req.body.username,
+      });
 
-  try {
-    let user = await User.findOne({
-      username: req.body.username,
-    });
+      if (user) {
+        return res.status(401).send({ msg: "Username already exists" });
+      }
 
-    if (user) {
-      return res.status(401).send({ msg: "Username already exists" });
+      user = new User.Admin({
+        ...req.body,
+        password: await hashPassword(req.body.password),
+      });
+
+      const token = createJWT(user);
+      bakeCookies(res, token);
+    } catch (error) {
+      res.status(401).send({ error: error });
     }
-
-    user = new User({
-      ...req.body,
-      password: await hashPassword(req.body.password),
-    });
-
-    const token = createJWT(user);
-    bakeCookies(res, token);
-  } catch (error) {
-    res.status(401).send({ error: error });
   }
-});
+);
 
 export default router;
---
+++ api/src/auth/validationSchema.ts
@@ -0,0 +1,18 @@
+import { check } from "express-validator";
+
+// change to yup
+export const LOGIN_VALIDATIONS = [
+  check("username", "Username is required").isString(),
+  check("password", "Password should be at least 8 characters").isLength({
+    min: 8,
+    max: 20,
+  }),
+];
+
+export const SIGNUP_VALIDATORS = [
+  check("username", "Username is required").isString(),
+  check("password", "Password should be at least 8 characters").isLength({
+    min: 8,
+    max: 20,
+  }),
+];

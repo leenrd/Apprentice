61d79e05ac31bde00b8b7cb8047c21b46c0a301c
Leenard <lolleenard0@gmail.com>
2024-04-14 14:59:25 +0800

    git hook trigger

--- commit-msg.sh
+++ commit-msg.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+echo "Checking commit message..."
+echo "Git Hook Triggered: commit-msg should be formatted as 'type: message' and not exceed 88 characters."
+echo "git message example: 'feat: add new feature', choose a type from: feat|fix|chore|docs|test|style|refactor|perf|build|ci|revert|enhancement"
+if [ ! -f "$1" ]; then
+    echo "Aborting commit. No commit message provided." >&2
+    exit 1
+fi
 if ! head -1 "$1" | grep -qE "^(feat|fix|chore|docs|test|style|refactor|perf|build|ci|revert|enhancement)(\(.+?\))?: .{1,}$"; then
     echo "Aborting commit. Your commit message is invalid." >&2
     exit 1
